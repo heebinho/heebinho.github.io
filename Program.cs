143f7d3c1312c4b05444d2943ef396d06785310a
Renato Heeb <renatoheeb@gmail.com>
2021-02-03 00:07:03 +0100

    Statiq setup

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Web;
+
+namespace heebinho.github.io
+{
+    class Program
+    {
+        public static async Task<int> Main(string[] args){
+            var task = await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .RunAsync();
+            return task;
+        }
+        
+    }
+}
