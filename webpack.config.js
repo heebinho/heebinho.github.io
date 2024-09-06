88dcecf7af9ce05353ec3857085ff05a40fbcf12
Renato Heeb <renatoheeb@gmail.com>
2024-09-06 20:08:46 +0200

    Switch from CJS to ESM

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+import path from "path";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 
-module.exports = {
+
+
+//const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+
+export default {
   entry: './src/assets/scripts/main.js',
   output: {
-    path: path.resolve(__dirname, '_site/assets'),
+    path: path.resolve('./', '_site/assets'),
     filename: 'main.js'
   },
   plugins: [new MiniCssExtractPlugin()],
